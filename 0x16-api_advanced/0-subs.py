cd7c61f3a96c0fd49b4e7ab427fa857c541f41cb
12siragi <siragisaidi218@gmail.com>
2024-06-20 13:12:24 -0700

    al

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+"""
+This module provides a function to get the number of subscribers for a subreddit.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Reddit API query by /u/yourusername)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit does not exist, it returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-agent': 'my-app-0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Data: {data}")  # Debugging line
+        return data['data'].get('subscribers', 0)
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return 0
